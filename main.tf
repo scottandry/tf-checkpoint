e4dad02d865ad52f7fdd736df5ff6c14b8cf70e6
Dennis Huynh <dennishuynh@protonmail.com>
2024-09-26 13:42:46 -0700

    Allows SSH connection, port 22

--- main.tf
+++ main.tf
@@ -26,3 +26,11 @@ resource "aws_security_group" "allow-ssh" {
   
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow-ssh" {
+  security_group_id = aws_security_group.allow-ssh.id
+
+  ip_protocol = "tcp"
+  from_port = 22
+  to_port = 22
+  cidr_ipv4 = "0.0.0.0/0"
+}
