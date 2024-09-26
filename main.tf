d254932ff2c82f8853bd85d38a50158dc7c84943
Dennis Huynh <dennishuynh@protonmail.com>
2024-09-26 13:40:43 -0700

    Provisions EC2 instance with custom security group

--- main.tf
+++ main.tf
@@ -10,3 +10,19 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_instance" "spring-boot-server" {
+  instance_type = "t2.micro"
+  ami = "ami-033067239f2d2bfbc"
+  key_name = "dh-sa-04-us-west-2"
+  security_groups = [ aws_security_group.allow-ssh.name ]
+
+  tags = {
+    "Name" = "dennis-tf-checkpoint-server"
+  }
+}
+
+resource "aws_security_group" "allow-ssh" {
+  
+}
+
