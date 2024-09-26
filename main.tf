8456c6bebc77acedae0933e0891c392097f1fdff
Dennis Huynh <dennishuynh@protonmail.com>
2024-09-26 14:08:00 -0700

    Via IAM, instance allowed S3 interaction

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "spring-boot-server" {
-  instance_type = "t2.micro"
-  ami = "ami-033067239f2d2bfbc"
-  key_name = "dh-sa-04-us-west-2"
-  security_groups = [ aws_security_group.allow-ssh.name ]
+  instance_type        = "t2.micro"
+  ami                  = "ami-033067239f2d2bfbc"
+  key_name             = "dh-sa-04-us-west-2"
+  security_groups      = [aws_security_group.allow-ssh.name]
   iam_instance_profile = aws_iam_instance_profile.allows-s3-interaction.name
 
   tags = {
@@ -24,25 +24,58 @@ resource "aws_instance" "spring-boot-server" {
 }
 
 resource "aws_security_group" "allow-ssh" {
-  
+
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow-ssh" {
   security_group_id = aws_security_group.allow-ssh.id
 
   ip_protocol = "tcp"
-  from_port = 22
-  to_port = 22
-  cidr_ipv4 = "0.0.0.0/0"
+  from_port   = 22
+  to_port     = 22
+  cidr_ipv4   = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow-all-traffic-outbound" {
   security_group_id = aws_security_group.allow-ssh.id
 
   ip_protocol = "-1"
-  cidr_ipv4 = "0.0.0.0/0"
+  cidr_ipv4   = "0.0.0.0/0"
 }
 
 resource "aws_iam_instance_profile" "allows-s3-interaction" {
-  
+  role = aws_iam_role.allows-s3-interaction.name
+}
+
+resource "aws_iam_role" "allows-s3-interaction" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "allows-s3-interaction" {
+  role = aws_iam_role.allows-s3-interaction.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:ListAllMyBuckets"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
